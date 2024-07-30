ac158b19c8c8f50e6b281c6df52661800b0795a3
Samuel Sastry <anonymouscamul@gmail.com>
2024-07-29 17:35:43 -0700

    added description to solver

---
+++ solver.py
@@ -0,0 +1,10 @@
+# general algo implementaion
+
+# For each ship, find all possible locations on the board where it could be placed.
+
+# loop until timer > 5sec:
+#     for random valid ship location:
+#         frequencies[ship's squares] += 1
+#     count += 1
+
+# guess = max(frequencies)
