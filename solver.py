a31690ca24dca6e9f16b4d3bc47c1fd3c8e02b92
pzliu <pzliu@berkeley.edu>
2024-07-30 21:13:10 -0700

    Updated Solver

--- solver.py
+++ solver.py
@@ -8,3 +8,112 @@
 #     count += 1
 
 # guess = max(frequencies)
+from classes import Board
+import random
+
+def solve_battleship(board):
+    possible_loc = {}
+
+    # For each ship from 1 to x
+    for ship_type in Board.types:
+        possible_loc[ship_type] = []
+        ship_length = Board.types[ship_type]
+        
+        # For each square from 1 to 100
+        for x in range(10):
+            for y in range(10):
+                
+                # For each orientation from vertical to horizontal
+                for orientation in ["horizontal", "vertical"]:
+                    pos = []
+                    if orientation == "horizontal":
+                        if x + ship_length > 10:
+                            continue
+                        pos = [(x+i, y) for i in range(ship_length)]
+                    else:
+                        if y + ship_length > 10:
+                            continue
+                        pos = [(x, y+i) for i in range(ship_length)]
+
+                    # If the ship would overhang the board, continue
+                    if any(s[0] >= 10 or s[1] >= 10 for s in pos):
+                        continue
+                    
+                    # If the ship would overlap a "miss" square, continue
+                    if any(board.grid[s[0]][s[1]] == 1 for s in pos):
+                        continue
+                    
+                    # Add this ship location to the list of possible locations
+                    possible_loc[ship_type].append(pos)
+
+    incompatible_loc = []
+
+    # For each ship
+    for ship_type in possible_loc:
+        
+        # For each possible ship location
+        for pos in possible_loc[ship_type]:
+            
+            # For every other ship
+            for other_ship_type in possible_loc:
+                if ship_type == other_ship_type:
+                    continue
+                
+                # For every possible other-ship location
+                for other_ship_squares in possible_loc[other_ship_type]:
+                    
+                    # If the two ships would overlap, save to list of incompatible locations
+                    if any(s in other_ship_squares for s in pos):
+                        incompatible_loc.append((pos, other_ship_squares))
+
+    location_freq = {ship_type: [0] * len(possible_loc[ship_type]) for ship_type in Board.types}
+    valid_cnt = 0
+
+    for _ in range(1000):  # Loop A, y times
+        selected_loc = {}
+        valid = True
+
+        # For each ship
+        for ship_type in possible_loc:
+            # Select one random possible ship location
+            idx = random.randint(0, len(possible_loc[ship_type]) - 1)
+            selected_location = possible_loc[ship_type][idx]
+
+            # If location is incompatible with any other selected locations, continue loop A
+            for loc in selected_loc.values():
+                if (selected_location, loc) in incompatible_loc or (loc, selected_location) in incompatible_loc:
+                    valid = False
+                    break
+            if not valid:
+                break
+
+            selected_loc[ship_type] = selected_location
+
+        if not valid:
+            continue
+
+        # If this configuration conflicts with the current board state
+        if any(board.grid[s[0]][s[1]] == 2 and s not in selected_location for s in selected_location for selected_location in selected_loc.values()):
+            continue
+
+        # For each selected ship location
+        for ship_type, selected_location in selected_loc.items():
+            location_freq[ship_type][possible_loc[ship_type].index(selected_location)] += 1
+        
+        valid_cnt += 1
+
+    square_freq = [[0 for _ in range(10)] for _ in range(10)]
+    
+    # For each possible ship location
+    for ship_type in possible_loc:
+        for index, pos in enumerate(possible_loc[ship_type]):
+            # For each square covered by this ship location
+            for s in pos:
+                square_freq[s[0]][s[1]] += location_freq[ship_type][index]
+    
+    # Divide each element in square_freq by valid_cnt
+    for x in range(10):
+        for y in range(10):
+            square_freq[x][y] /= valid_cnt
+
+    return square_freq
