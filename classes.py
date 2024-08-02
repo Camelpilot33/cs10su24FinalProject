7fd792eccb6cd499958c75c492b6046873c4fa2a
Samuel Sastry <anonymouscamul@gmail.com>
2024-08-01 21:03:14 -0700

    type annotated classes.py

--- classes.py
+++ classes.py
@@ -1,18 +1,18 @@
 import random
-import colors
+from colors import color as clr
 
 
-# class for each players' board
+# Board class
 class Board:
     # The board will be printed with these labels:
     # 0: empty, 1: miss, 2: hit, 3: ship
-    labels = {
-        0: colors.color.OKBLUE + "." + colors.color.ENDC,
-        1: colors.color.BOLD + "O" + colors.color.ENDC,
-        2: colors.color.FAIL + "X" + colors.color.ENDC,
-        3: colors.color.WARNING + "#" + colors.color.ENDC,
+    labels: dict[int, str] = {
+        0: clr.OKBLUE + "." + clr.ENDC,
+        1: clr.BOLD + "O" + clr.ENDC,
+        2: clr.FAIL + "X" + clr.ENDC,
+        3: clr.WARNING + "#" + clr.ENDC,
     }
-    types = {
+    types: dict[str, int] = {
         "Destroyer": 2,
         "Submarine": 3,
         "Cruiser": 3,
@@ -20,16 +20,16 @@ class Board:
         "Carrier": 5,
     }
 
-    def __init__(self):
+    def __init__(self: "Board") -> None:
         """
         Initializes a new instance of the class.
 
         The __init__ method is called when a new object of the class is created. It initializes the grid attribute as a 10x10 matrix filled with zeros and the ships attribute as an empty list.
         """
-        self.grid = [[0 for x in range(10)] for y in range(10)]
+        self.grid: list[list[int]] = [[0 for x in range(10)] for y in range(10)]
         self.ships: list[Ship] = []
 
-    def hit(self: 'Board', square: tuple[int, int]) -> bool:
+    def hit(self: "Board", square: tuple[int, int]) -> bool:
         """
         Checks if the guess was a hit and updates the board accordingly.
 
@@ -39,19 +39,19 @@ class Board:
         Returns:
         - bool: True if the guess was a hit, False otherwise.
         """
-        gotHit = False
+        gotHit: bool = False
         for ship in self.ships:
             if ship.isHit(square):
                 gotHit = True
 
-        if gotHit:
+        if gotHit: # hit
             self.grid[square[0]][square[1]] = 2
             return True
-        else:
+        else: # miss
             self.grid[square[0]][square[1]] = 1
             return False
 
-    def gameOver(self):
+    def gameOver(self: "Board") -> bool:
         """
         Checks if the game is over.
 
@@ -63,7 +63,7 @@ class Board:
                 return False
         return True
 
-    def placeShip(self, ship):
+    def placeShip(self: "Board", ship: "Ship") -> bool:
         """
         Places a ship on the board.
 
@@ -74,16 +74,16 @@ class Board:
         - True if the ship was successfully placed, False otherwise.
         """
         for s in ship.squares:
-            if s[0] < 0 or s[0] > 9 or s[1] < 0 or s[1] > 9:
+            if s[0] < 0 or s[0] > 9 or s[1] < 0 or s[1] > 9: # out of bounds
                 return False
             for otherShip in self.ships:
                 for otherSquare in otherShip.squares:
-                    if s == otherSquare:
+                    if s == otherSquare: # overlapping
                         return False
         self.ships.append(ship)
         return True
 
-    def placeShipRandom(self, shipType, triesLeft=100):
+    def placeShipRandom(self: "Board", shipType: str, triesLeft: int=100) -> bool:
         """
         Places a ship on the board at random coordinates.
 
@@ -93,29 +93,29 @@ class Board:
         Returns:
         - True if the ship was successfully placed, False otherwise.
         """
-        ship = Ship([(0, 0) for i in range(Board.types[shipType])], shipType)
+        ship: "Ship" = Ship([(0, 0) for _ in range(Board.types[shipType])], shipType)
 
         # Randomly choose the orientation of the ship
-        orientation = random.choice(["horizontal", "vertical"])
+        orientation: str = random.choice(["horizontal", "vertical"])
         if orientation == "horizontal":
-            x = random.randint(0, 9 - len(ship.squares))
-            y = random.randint(0, 9)
+            x: int = random.randint(0, 9 - len(ship.squares))
+            y: int = random.randint(0, 9)
             for i in range(len(ship.squares)):
                 ship.squares[i] = (x + i, y)
         else:
-            x = random.randint(0, 9)
-            y = random.randint(0, 9 - len(ship.squares))
+            x: int = random.randint(0, 9)
+            y: int = random.randint(0, 9 - len(ship.squares))
             for i in range(len(ship.squares)):
                 ship.squares[i] = (x, y + i)
 
-        if not self.placeShip(ship) and triesLeft > 0:
+        if not self.placeShip(ship) and triesLeft > 0: # try again
             return self.placeShipRandom(shipType, triesLeft - 1)
-        elif triesLeft == 0:
+        elif triesLeft == 0: # give up
             return False
         return True
 
     # Print the board
-    def stringify(self, selected=None, drawShips=True):
+    def stringify(self: "Board", selected: tuple|None=None, drawShips: bool=True) -> str:
         """
         Returns a string representation of the board.
 
@@ -126,7 +126,7 @@ class Board:
             str: A string representation of the board.
         """
         # Copy the grid
-        numBoard = [[i for i in row] for row in self.grid]
+        numBoard: list[list[int]] = [[i for i in row] for row in self.grid]
         # Mark the ships
         if drawShips:
             for ship in self.ships:
@@ -136,43 +136,37 @@ class Board:
 
         # add coordinates
         if selected is not None:
-            r, c = selected
-            printBoard = (
-                "  1 2 3 4 5 6 7 8 9 10\n"
-                + colors.color.ENDC
-            )
+            r,c = selected
+            printBoard: str = "  1 2 3 4 5 6 7 8 9 10\n" + clr.ENDC
             for i, row in enumerate(numBoard):
                 printBoard += f"{chr(i+65)} "
                 for j, cell in enumerate(row):
                     if (i, j) == (r, c):
-                        printBoard += f"\b{colors.color.BOLD+colors.color.HL}[{Board.labels[cell]}{colors.color.BOLD+colors.color.HL}]{colors.color.ENDC}"
+                        printBoard += f"\b{clr.BOLD+clr.HL}[{Board.labels[cell]}{clr.BOLD+clr.HL}]{clr.ENDC}"
                     else:
                         printBoard += f"{Board.labels[cell]}"
                         # if j+1 != c or r != i:
                         printBoard += " "
                 printBoard += "\n"
         else:
-            printBoard = (
-                "  1 2 3 4 5 6 7 8 9 10\n"
-                + "\n".join(
-                    [
-                        f"{chr(i+65)} "
-                        + " ".join([Board.labels[cell] for cell in row])
-                        + " "
-                        for (i, row) in enumerate(numBoard)
-                    ]
-                )
+            printBoard = "  1 2 3 4 5 6 7 8 9 10\n" + "\n".join(
+                [
+                    f"{chr(i+65)} "
+                    + " ".join([Board.labels[cell] for cell in row])
+                    + " "
+                    for (i, row) in enumerate(numBoard)
+                ]
             )
 
         return printBoard
 
-    def __str__(self):
+    def __str__(self: "Board") -> str:
         return self.stringify(None)
 
 
 # Ship class
 class Ship:
-    def __init__(self, squares, shipType):
+    def __init__(self: "Ship", squares: list[tuple[int,int]], shipType: str) -> None:
         """
         Initializes a Ship object.
 
@@ -185,10 +179,10 @@ class Ship:
         - sunk (bool): Indicates whether the ship is sunk or not.
         - shipType (string): The type of the ship.
         """
-        self.squares = squares  # list of tuples
-        self.shipType = shipType  # string
+        self.squares = squares
+        self.shipType = shipType
 
-    def isSunk(self, board):
+    def isSunk(self: "Ship", board: "Board") -> bool:
         """
         Checks if the ship is sunk.
 
@@ -200,7 +194,7 @@ class Ship:
                 return False
         return True
 
-    def isHit(self, square):
+    def isHit(self: "Ship", square: tuple[int, int]) -> bool:
         """
         Checks if a given square is hit.
 
@@ -211,21 +205,3 @@ class Ship:
         - True if the square is hit, False otherwise.
         """
         return square in self.squares
-
-
-# # # Tests
-# board = Board()
-# ship = Ship([(1, 1), (1, 2), (1, 3)], "Destroyer")
-# # ship1 = Ship([(4, 2), (4, 3), (4, 4), (4, 5)], "Battleship")
-# (board.placeShip(ship))
-# # (board.placeShip(ship1))
-# # (board.placeShipRandom("Carrier"))
-# # (board.gameOver())
-# board.hit((1, 1))
-# board.hit((1, 2))
-# board.hit((2, 6))
-# # (board.gameOver())
-# print(board.stringify((1, 1),False))
-# # # print(">"+board.stringify()[0:47]+"<")
-# # print(len(board.stringify().split("\n")[2]) + 1)
-# # print(board.stringify((5, 5)))
