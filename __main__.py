5210a1767ff9aa0deee94a9d968fa493b798691e
Samuel Sastry <anonymouscamul@gmail.com>
2024-08-05 18:41:23 -0700

    Revert "added instructions"
    This reverts commit 3fd65f361dd75db1835a5a38e4b6cc09a58dbdaa.

--- __main__.py
+++ __main__.py
@@ -1,7 +1,3 @@
-# HOW TO RUN THE GAME:
-# after adding requirements (see instructions)
-# run `py __main__.py` or `python3 __main__.py`
-
 from pynput import keyboard # needed for key input handling
 import os # needed for clearing the console
 import sys # needed for checking the operating system
